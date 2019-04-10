ab2208ab4325b5ca7c71f2feb689fbe1975799a4
Timur <timurbikmullin@mail.ru>
2019-04-10 19:40:13 +0300

    Maven

---
+++ SummerPractise/Maven/src/main/java/Main/TestMaven.java
@@ -0,0 +1,15 @@
+package main.java.Main;
+
+
+import javax.swing.*;
+
+public class TestMaven {
+
+    public static void main(String[] args) {
+        JFrame jf = new JFrame("Hello World");
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setSize(300, 400);
+        jf.setVisible(true);
+
+    }
+}
